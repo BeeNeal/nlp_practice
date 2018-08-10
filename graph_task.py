ccee5e18a6ee7408c72fd0cef4c4451cef483c2b
Brittany Neal <brittanyneal22@gmail.com>
2018-08-10 16:18:42 -0700

    working on adding POS to node instances

--- graph_task.py
+++ graph_task.py
@@ -5,15 +5,15 @@ import nltk
 def removed_punctuation(s):
     """takes in a string and removes all punctuation  """
 
-    punctuation = set(['.', ','])
-    for char in s:
-        if char in
+    # punctuation = set(['.', ','])
+    # for char in s:
+    #     if char in
 
 def tokenize(s):
     """Takes in a string, and puts it into a list of strings based on spaces
     
     >>> tokenize('This is a very practical sentence')
-    ['This', ' is', ' a', ' very', ' practical', ' sentence']
+    ['This', 'is', 'a', 'very', 'practical', 'sentence']
 
     """
 
@@ -28,18 +28,33 @@ def tokenize(s):
         elif s[i] == ' ':
             word = s[index:i]
             tokenized_sent.append(word)
-            index = i
+            index = i + 1  # incrementing index cuts off the space
 
     return tokenized_sent
 
 
-class WordNode():
+class WordNode(object):
     """Word node for graph"""
 
     def __init__(self, word, left=None, right=None):
         self.word = word 
         self.left = left
         self.right = right
+        self.pos = None
+
+    def assign_pos(self):
+        """Uses nltk pos tagger and assigns POS
+
+        """
+
+        pos = nltk.pos_tag(self.word)
+        self.pos = pos[1]
+
+        # FIXME the below is a test
+        # >>> print(assign_pos('blueberry'))
+        # 'NN'
+        # pos = nltk.pos_tag(word)
+        # self.pos = pos[1]
 
     def insert_left(self, left):
         self.left = left
@@ -53,13 +68,30 @@ s = 'This is a very practical sentence'
 def lines_to_graph(sent):
     """Takes lines/sents/phrases and makes graphs from them"""
 
+    sentence_graph = []
     sent = tokenize(s)
-    
 
-for word in sent:
+    sent_head = WordNode(sent[0], None, sent[1])
+    sentence_graph.append(sent_head)
 
+    for i in range(1, len(sent) - 1):
+        w = WordNode(sent[i], sent[i-1], sent[i+1])
+        sentence_graph.append(w)
 
+    sent_tail = WordNode(sent[-1], sent[-2])
+    sentence_graph.append(sent_tail)
 
+    return sentence_graph
+
+# print (tokenize(s))
+sample = lines_to_graph(s)
+for word in sample:
+    print(word.word, word.right)
+
+x = WordNode('blueberry')
+print(x)
+WordNode.assign_pos
+print(x.pos)
 
 
 
